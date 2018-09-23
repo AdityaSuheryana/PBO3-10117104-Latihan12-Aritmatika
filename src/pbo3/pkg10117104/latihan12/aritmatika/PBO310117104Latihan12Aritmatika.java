fda886923adead896e1e5c2c52eb2a5a6f9ad39b
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 19:50:01 +0700

    First Commit

---
+++ src/pbo3/pkg10117104/latihan12/aritmatika/PBO310117104Latihan12Aritmatika.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan12.aritmatika;
+
+/**
+ *
+ * @author  
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan fungsi operator Aritmatika
+ */
+public class PBO310117104Latihan12Aritmatika {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("b / a = "+(b/a));
+        System.out.println("b % a = "+(b%a));
+        System.out.println("c % a = "+(c%a));
+        System.out.println("a++ = "+(a++));
+        System.out.println("b-- = "+(b--));
+        System.out.println("d++ = "+(d++));
+        System.out.println("++d = "+(++d));
+        
+    }
+    
+}
